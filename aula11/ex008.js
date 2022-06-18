cd67338598382b28a5cd522999377579f3043655
VICTOR-AZZOLINI-DEV <99159163+VICTOR-AZZOLINI-DEV@users.noreply.github.com>
2022-06-18 12:02:47 -0300

    Fiz exercícios Javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do ser carro é ${vel}Km/h `)
+if (vel > 60) { //Condição simples
+    console.log(`Você ultrapassou a velocidade permitida MULTADO!`)
+}
+console.log(`Digira sempre usando cinto de segurança!`)
